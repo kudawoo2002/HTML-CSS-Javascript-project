96332d03a66001f4590a68e1b927437c6222871c
Kwami Kudawoo <kudawoo2002@gmail.com>
2026-05-27 14:52:59 +0000

    add input field and update js,css file

--- pomodoro-timer/index.js
+++ pomodoro-timer/index.js
@@ -2,39 +2,52 @@ const btnStartEl = document.getElementById("btn-start");
 const btnStopEl = document.getElementById("btn-stop");
 const btnResetEl = document.getElementById("btn-reset");
 const timerEl = document.getElementById("timer");
+const timerInputEl = document.getElementById("timer-input");
 
 let interval;
-let timerLeft = 1500;
+let stopTime = 0;
 
 function updateTimer() {
-  let m = Math.floor(timerLeft / 60);
-  let s = timerLeft % 60;
+  h = Math.floor(timerLeft / 3600);
+  m = Math.floor((timerLeft % 3600) / 60);
+  s = Math.floor(timerLeft % 60);
 
-  let timerFormat = `${m < 10 ? "0" + m : m}:${s < 10 ? "0" + s : s}`;
+  let timerFormat = `${h.toString().padStart(2, "0")}h:${m < 10 ? "0" + m : m}m:${s < 10 ? "0" + s : s}s`;
 
   timerEl.innerText = timerFormat;
   timerLeft--;
+
   if (timerLeft === 0) {
     clearInterval(interval);
-    timerLeft = 1500;
+    timerInputEl;
     updateTimer();
+    timerInputEl.value = "";
   }
 }
 
 function startTimer() {
-  interval = setInterval(() => {
-    updateTimer();
-  }, 1000);
+  if (timerInputEl.value === "") {
+    alert("Enter number");
+  } else {
+    timerLeft = Number(timerInputEl.value);
+
+    interval = setInterval(() => {
+      updateTimer();
+    }, 1000);
+  }
 }
 
 function stopTimer() {
   clearInterval(interval);
+  stopTime = timerLeft;
+  timerInputEl.value = stopTime;
 }
 
 function resetTimer() {
   clearInterval(interval);
-  timerLeft = 1500;
+  timerInputEl.value = "";
   updateTimer();
+  timerEl.innerText = "00:00:00";
 }
 
 btnStartEl.addEventListener("click", startTimer);
